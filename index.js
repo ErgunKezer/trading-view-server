b26247a9eedd0fce738f7884b91782479fcfa406
Ergun Kezer <ekzr34@gmail.com>
2021-05-22 15:13:10 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+const cors = require('cors');
+app.use(cors());
+
+const morgan = require('morgan');
+app.use(morgan('tiny'));
+
+const proxy = require('express-http-proxy');
+app.use('/', proxy('https://api.binance.com'));
+
+app.listen(port, () => {
+    console.log('Listening on 3001');
+});
