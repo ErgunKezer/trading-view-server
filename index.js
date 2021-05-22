233482ef786895a821201eb9ca4f0a0d3440c16e
Ergun Kezer <ekzr34@gmail.com>
2021-05-22 19:43:29 +0300

    fix 2

--- index.js
+++ index.js
@@ -3,20 +3,17 @@ const app = express();
 const port = process.env.PORT || 3001;
 const cors = require('cors');
 const morgan = require('morgan');
+const proxy = require('express-http-proxy');
 
-app.use(
-    cors({
-        origin: '*',
-        methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
-        allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Authorization'],
-        credentials: true,
-    })
-);
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Authorization'],
+    credentials: true,
+};
 
 app.use(morgan('tiny'));
-
-const proxy = require('express-http-proxy');
-app.use('/', proxy('https://api.binance.com'));
+app.use('/', cors(corsOptions), proxy('https://api.binance.com'));
 
 app.listen(port, () => {
     console.log('Listening on' + port);
