c63b56326e74ece0ac38c0b7d51e94e0ed9105d4
Ergun Kezer <ekzr34@gmail.com>
2021-05-22 19:45:33 +0300

    revert

--- index.js
+++ index.js
@@ -2,18 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 const cors = require('cors');
+app.use(cors());
+
 const morgan = require('morgan');
-const proxy = require('express-http-proxy');
-
-const corsOptions = {
-    origin: '*',
-    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Authorization'],
-    credentials: true,
-};
-
 app.use(morgan('tiny'));
-app.use('/', cors(corsOptions), proxy('https://api.binance.com'));
+
+const proxy = require('express-http-proxy');
+app.use('/', proxy('https://api.binance.com'));
 
 app.listen(port, () => {
     console.log('Listening on' + port);
